1c157a3b418d0ef78f24d53b0b82c15f38d388f9
Christian Lionel Kimbembe <clkimbembe@tribvn-hc.com>
2022-10-02 21:34:49 +0200

    Init project with posts get in index

---
+++ src/reducers/user.reducer.js
@@ -0,0 +1,15 @@
+import { GET_USERS } from "../actions/user.action";
+
+const initialState = {}
+
+export default function postReducer(state = initialState, action) {
+    switch (action.type) {
+        case GET_USERS:
+            return action.payload;
+
+        default:
+            return state;
+    }
+
+    return initialState;
+}
